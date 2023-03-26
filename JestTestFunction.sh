6ef14a9256f7d78f192a5320255f8dbe52645519
GrahamPosey <82821975+GrahamPosey@users.noreply.github.com>
2023-03-25 21:19:28 -0400

    Update JestTestFunction.sh

--- JestTestFunction.sh
+++ JestTestFunction.sh
@@ -1,16 +1,15 @@
 ###############################Clone the repo passed as a param#######################################
 copado -p 'Cloning the repository' 
-#mkdir JestProject
-#cd JestProject
 git clone $repo -b "$branch" .
 sfdx force:config:set instanceUrl=https://slalomorg.my.salesforce.com --global
 #copado-git-get -d . "$branch"
+
 ###############################Installing jest libs via npm#########################
 copado -p 'Installing jest'
 npm init -y
 sfdx force:lightning:lwc:test:setup
 node --experimental-vm-modules ./node_modules/jest/bin/jest.js
-echo "Before Version"
+
 ##############################Setting LWCs to run jest on################################
 copado -p 'Retrieveing LWCs to test'
 jestCommandString=""
@@ -20,6 +19,7 @@ do
 	individualLwcPath="$pathToLwcs${element}/__tests__/${element}.test.js"
 	jestCommandString="$jestCommandString $individualLwcPath "
 done
+
 ###############################Run all jest tests in cloned repo######################################
 copado -p 'Running Jest Suite'
 ./node_modules/.bin/jest --findRelatedTests $jestCommandString --outputFile=/tmp/result.json --json --testFailureExitCode 0
@@ -28,12 +28,14 @@ cat /tmp/result.json | python -m json.tool > /tmp/jestResults.json
 ###############################Attach the test results to result record###############################
 copado -p 'Attaching Result File' 
 copado --uploadfile /tmp/jestResults.json
+
 #######################Determine if the tests passed############
+copado -p 'Calculating Failed Test Count'
 failedTestCount=$(jq '.numFailedTestSuites' /tmp/jestResults.json)
-copado -p 'Determining failed test count'
-echo $failedTestCount
+
 ########################Write result back to user story##########################
-#sfdx force:config:set instanceUrl=https://login.salesforce.com --global
+
+copado -p 'Updating User Story w/ Results'
 if [ "$failedTestCount" -gt "$failedTestLimit" ]
 then
 	jestResult='Fail'
